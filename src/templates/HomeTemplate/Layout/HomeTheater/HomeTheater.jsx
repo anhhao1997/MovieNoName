1b2cf7699409b80e2a98d2182a99d846a774893f
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-06-06 12:20:08 +0700

    'Loc_End_HomeTheater'

--- src/templates/HomeTemplate/Layout/HomeTheater/HomeTheater.jsx
+++ src/templates/HomeTemplate/Layout/HomeTheater/HomeTheater.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
-import { Tabs } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { layDanhSachHeThongRap } from "../../../../redux/actions/QuanLyRapActions";
-import { NavLink } from "react-router-dom";
-import moment from "moment";
+import HomeTheaterTabPane from "./HomeTheaterTabPane";
 
 export default function HomeTheater(props) {
-  const { TabPane } = Tabs;
-
+  
   const { heThongRapChieu } = useSelector((state) => state.QuanLyRapReducer);
 
   const dispatch = useDispatch();
@@ -18,70 +15,11 @@ export default function HomeTheater(props) {
     dispatch(action);
   }, []);
 
-  const renderHeThongRap = () => {
-    return heThongRapChieu.map((heThongRap, index) => {
-      return (
-        //render logo heThongRap
-        <TabPane key={index} tab={<img src={heThongRap.logo} className="rounded-full" width="50" />}>
-          <Tabs tabPosition={"left"}>
-            {/* render tiep cumRap từ lstCumRap */}
-            {heThongRap.lstCumRap?.slice(0, 5).map((cumRap, index) => {
-              return (
-                <TabPane
-                  key={index}
-                  tab={
-                    <div className="text-left" style={{ width: "380px" }}>
-                      <div className="flex gap-2 items-center">
-                        <img src={heThongRap.logo} className="rounded-full" width="40" />
-                        <div className=" uppercase font-semibold">{cumRap.tenCumRap}</div>
-                      </div>
-
-                      <p className="mt-1">Địa chỉ: {cumRap.diaChi.length > 50 ? cumRap.diaChi.slice(0, 45) + " ..." : cumRap.diaChi}</p>
-                    </div>
-                  }
-                >
-                  {/* render danhSachPhim tuong ứng */}
-                  {cumRap.danhSachPhim.slice(0, 5).map((phim, index) => {
-                    return (
-                      <div className="mb-2" key={index}>
-                        <div className="flex m-1 gap-2 py-2 text-xs font-semibold border-b-2 border-gray-200 transition ease-in-out delay-150 hover:shadow-md shadow-black hover:scale-110 duration-300 hover:border-2 ">
-                          {/* hinhAnhPhim */}
-                          <div className="shadow-md shadow-black border-8 border-white">
-                            <img src={phim.hinhAnh} alt={phim.tenPhim} width="150" style={{ height: "100%" }} />
-                          </div>
-                          {/* tenPhim */}
-                          <div className="relative">
-                            <h1 className="w-fit uppercase border-l-8 border-black p-1 bg-red-500 text-white">{phim.tenPhim}</h1>
-
-                            {/* render lstLichChieuTheoPhim */}
-                            <div className="grid grid-cols-6 gap-2">
-                              {phim.lstLichChieuTheoPhim?.slice(0, 10).map((lichChieu, index) => {
-                                return (
-                                  <NavLink className="text-black bg-gray-50 border-2 p-2 hover:text-white hover:bg-red-500" to="/" key={index}>
-                                    {moment(lichChieu.ngayChieuGioChieu).format("hh:mm")}
-                                  </NavLink>
-                                );
-                              })}
-                            </div>
-
-                            <button className="bg-orange-500 text-white px-3 py-2 mt-3 absolute bottom-2">Đặt vé ngay</button>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </TabPane>
-              );
-            })}
-          </Tabs>
-        </TabPane>
-      );
-    });
-  };
+ 
 
   return (
-    <div className="container my-5 w-4/5">
-      <Tabs tabPosition={"left"}>{renderHeThongRap()}</Tabs>
+    <div className="container">
+      <HomeTheaterTabPane heThongRapChieu={heThongRapChieu}/>
     </div>
   );
 }
---
+++ src/templates/HomeTemplate/Layout/HomeTheater/HomeTheaterTabPane.jsx
@@ -0,0 +1,76 @@
+import { Tabs } from "antd";
+import React from "react";
+import moment from "moment";
+import { NavLink } from "react-router-dom";
+
+export default function HomeTheaterTabPane(props) {
+  const { TabPane } = Tabs;
+  const renderHeThongRap = () => {
+    return props.heThongRapChieu.map((heThongRap, index) => {
+      return (
+        //render logo heThongRap
+        <TabPane key={index} tab={<img src={heThongRap.logo} className="rounded-full" width="50" />}>
+          <Tabs tabPosition={"top"}>
+            {/* render tiep cumRap từ lstCumRap */}
+            {heThongRap.lstCumRap?.slice(0, 5).map((cumRap, index) => {
+              return (
+                <TabPane
+                  key={index}
+                  tab={
+                    <div className="text-left" style={{ width: "380px" }}>
+                      <div className="flex gap-2 items-center">
+                        <img src={heThongRap.logo} className="rounded-full" width="40" />
+                        <div className=" uppercase font-semibold">{cumRap.tenCumRap}</div>
+                      </div>
+
+                      <p className="mt-1">Địa chỉ: {cumRap.diaChi.length > 50 ? cumRap.diaChi.slice(0, 45) + " ..." : cumRap.diaChi}</p>
+                    </div>
+                  }
+                >
+                  {/* render danhSachPhim tuong ứng */}
+                  {cumRap.danhSachPhim.slice(0, 5).map((phim, index) => {
+                    return (
+                      <div
+                        className="flex m-1 p-2 gap-2 text-xs font-semibold border-b-2 border-gray-200 transition ease-in-out delay-150 hover:shadow-md shadow-black hover:scale-105 duration-300 hover:border-2 "
+                        key={index}
+                      >
+                        {/* hinhAnhPhim */}
+                        <div className="shadow-md shadow-black border-8 border-white">
+                          <img src={phim.hinhAnh} alt={phim.tenPhim} width="150" style={{ height: "100%" }} />
+                        </div>
+                        {/* tenPhim */}
+                        <div className="relative">
+                          <h1 className="w-fit uppercase border-l-8 border-black p-1 mb-2 bg-red-500 text-white">{phim.tenPhim}</h1>
+
+                          {/* render lstLichChieuTheoPhim */}
+                          <div className="grid grid-cols-6 gap-2">
+                            {phim.lstLichChieuTheoPhim?.slice(0, 10).map((lichChieu, index) => {
+                              return (
+                                <NavLink className="text-black bg-gray-50 border-2 p-2 hover:text-white hover:bg-red-500" to="/" key={index}>
+                                  {moment(lichChieu.ngayChieuGioChieu).format("hh:mm")}
+                                </NavLink>
+                              );
+                            })}
+                          </div>
+
+                          <button className="bg-orange-500 text-white px-3 py-2 absolute bottom-1">Đặt vé ngay</button>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </TabPane>
+              );
+            })}
+          </Tabs>
+        </TabPane>
+      );
+    });
+  };
+  console.log("heThongRapChieu", props);
+  return (
+    <div className="my-10">
+      <h2 className="uppercase text-center text-3xl font-semibold my-3">HỆ THỐNG RẠP CHIẾU</h2>
+      <Tabs centered tabPosition={"top"}>{renderHeThongRap()}</Tabs>
+    </div>
+  );
+}
