a45b69f2dcf084a432dca7e0baaa5f44bcfe45b6
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-06-07 21:41:35 +0700

    'Loc_Edit_DetailFilmComponent'

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -5,10 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { layThongTinLichChieuPhim } from "../../redux/actions/QuanLyRapActions";
 
 export default function Detail(props) {
-// bóc tách filmDetail trong QuanLyPhimReducer
+  // bóc tách filmDetail trong QuanLyPhimReducer
   const filmDetail = useSelector((state) => state.QuanLyPhimReducer.filmDetail);
 
-//Khi vừa load lên thì dispatch(action) layThongTinLichChieuPhim thông qua id được lấy từ param trên url
+  //Khi vừa load lên thì dispatch(action) layThongTinLichChieuPhim thông qua id được lấy từ param trên url
   const dispatch = useDispatch();
   useEffect(() => {
     //lấy id từ param trên url
@@ -16,14 +16,35 @@ export default function Detail(props) {
     // console.log(id);
     const action = layThongTinLichChieuPhim(id);
     dispatch(action);
-  },[]);
+  }, []);
 
   return (
-    <div style={{ backgroundImage: `url(${filmDetail.hinhAnh})`, minHeight: "100vh" }} className="bg-cover bg-center bg-no-repeat w-full">
-      <div className="glassmorphism glassmorphism-black pt-10" style={{ minHeight: "100vh" }}>
-        <DetailInfoFilm filmDetail={filmDetail}></DetailInfoFilm>
-        <DetailTheater></DetailTheater>
+    <div>
+      <div style={{ backgroundImage: `url(${filmDetail.hinhAnh})`, minHeight: "100vh" }} className="bg-cover bg-center bg-no-repeat w-full">
+        <div className="glassmorphism-black pt-10" style={{ minHeight: "100vh" }}>
+          <DetailInfoFilm filmDetail={filmDetail}></DetailInfoFilm>
+          <DetailTheater></DetailTheater>
+        </div>
       </div>
+
+
+      <div className="modal fade bd-example-modal" id="exampleModalCenter" tabIndex={-1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content modal-trailer">
+              <div className="modal-header ">
+                <h2 className="modal-title font-semibold text-lg " id="exampleModalLongTitle">
+                  {filmDetail.tenPhim}
+                </h2>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <iframe className="h-full w-full" src={filmDetail.trailer}></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
--- src/templates/HomeTemplate/Layout/Header/Header.jsx
+++ src/templates/HomeTemplate/Layout/Header/Header.jsx
@@ -1,42 +1,56 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header(props) {
+  return (
+    //Có chỉnh sửa phần header
 
-    return (
-
-        //Có chỉnh sửa phần header
-
-        <div className='fixed w-full text-white glassmorphism-black drop-shadow-2xl top-0 z-10'>
-            <header className='container mx-auto'>
-                <nav className="navbar navbar-expand-lg navbar-dark">
-                    <NavLink to="#">Logo</NavLink>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <div className='nav-content'>
-                                <NavLink className='content-item' to="/">Lịch chiếu phim</NavLink>
-                                <NavLink className='content-item' to="/" >Liên hệ</NavLink>
-                                <NavLink className='content-item' to="/">Tin tức</NavLink>
-                            </div>
-                            <div className='nav-mode'>
-                                <span className='mode light-mode'>
-                                    <i class="fa fa-sun"></i>
-                                </span>
-                                <span className='mode dark-mode'>
-                                    <i class="fa fa-moon"></i>
-                                </span>
-                            </div>
-                            <div className='nav-login'>
-                                <NavLink to='/' className='login-item'>
-                                    <i class="fa fa-user pr-2"></i>Đăng kí / Đăng nhập</NavLink>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
-        </div>
-    )
+    <div className="fixed w-full text-white glassmorphism-black drop-shadow-2xl top-0 z-10">
+      <header className="container mx-auto">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <NavLink to="#">Logo</NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <div className="nav-content">
+                <NavLink className="content-item" to="/">
+                  Lịch chiếu phim
+                </NavLink>
+                <NavLink className="content-item" to="/">
+                  Liên hệ
+                </NavLink>
+                <NavLink className="content-item" to="/">
+                  Tin tức
+                </NavLink>
+              </div>
+              <div className="nav-mode">
+                <span className="mode light-mode">
+                  <i className="fa fa-sun" />
+                </span>
+                <span className="mode dark-mode">
+                  <i className="fa fa-moon" />
+                </span>
+              </div>
+              <div className="nav-login">
+                <NavLink to="/" className="login-item">
+                  <i className="fa fa-user pr-2" />
+                  Đăng kí / Đăng nhập
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 }
