aa0137a392366b92afdf804c6e3e012667f47865
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-06-15 14:58:22 +0700

    'Loc_XuLyDatGhe_CSS_CheckoutComponent'

--- src/redux/actions/QuanLyDatVeActions.js
+++ src/redux/actions/QuanLyDatVeActions.js
@@ -1,11 +1,6 @@
 import { quanLyDatVeService } from "../../services/QuanLyDatVeService";
-<<<<<<< HEAD
 import { ThongTinDatVe } from "../../_core/models/ThongTinDatVe";
 import { SET_CHI_TIET_PHONG_VE } from "./../types/QuanLyDatVeType";
-=======
-import { DAT_VE_HOAN_TAT, SET_CHI_TIET_PHONG_VE } from "./../types/QuanLyDatVeType";
-import { displayLoadingAction, hideLoadingAction } from "./LoadingActions";
->>>>>>> 92ff7af08b79c8a8729ae399e884c2b8192071b2
 
 export const layChiTietPhongVeAction = (maLichChieu) => {
   return async (dispatch) => {
@@ -23,7 +18,6 @@ export const layChiTietPhongVeAction = (maLichChieu) => {
       console.log("error", error);
       console.log("error", error.response?.data);
     }
-<<<<<<< HEAD
   };
 };
 
@@ -37,31 +31,3 @@ export const datVeAction = (thongTinDatVe = new ThongTinDatVe()) => {
     }
   };
 };
-=======
-}
-
-export const datVeAction = (thongTinDatVe) => {
-    return async (dispatch) => {
-        try {
-
-            dispatch(displayLoadingAction)
-
-            const result = await quanLyDatVeService.datVe(thongTinDatVe);
-            // đặt vé thành công thì cập nhật
-            await dispatch(layChiTietPhongVeAction(thongTinDatVe.maLichChieu))
-
-            await dispatch({
-                type: DAT_VE_HOAN_TAT
-            })
-
-            dispatch(hideLoadingAction)
-            // console.log('result', result.data.content)
-
-        } catch (error) {
-            dispatch(hideLoadingAction)
-            console.log('error', error);
-            console.log('error', error.response?.data);
-        }
-    }
-}
->>>>>>> 92ff7af08b79c8a8729ae399e884c2b8192071b2
--- src/redux/types/QuanLyDatVeType.js
+++ src/redux/types/QuanLyDatVeType.js
@@ -1,7 +1,2 @@
 export const SET_CHI_TIET_PHONG_VE = "SET_CHI_TIET_PHONG_VE";
-<<<<<<< HEAD
 export const DAT_VE = "DAT_VE";
-=======
-export const DAT_GHE = "DAT_GHE";
-export const DAT_VE_HOAN_TAT = "DAT_VE_HOAN_TAT";
->>>>>>> 92ff7af08b79c8a8729ae399e884c2b8192071b2
