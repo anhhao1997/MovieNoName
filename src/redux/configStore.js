66eebdc05e2d2be07161709376a9cead7fb94277
anhhao <voanhhao9.1997@gmail.com>
2022-07-22 17:31:36 +0700

    Hao_pull

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -20,6 +20,7 @@ const rootReducer = combineReducers({
 // Cấu hình thunk
 const middleWare = applyMiddleware(thunk);
 
-const customCompose = compose(middleWare, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const customCompose = compose(middleWare);
+//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export const store = createStore(rootReducer, customCompose);
